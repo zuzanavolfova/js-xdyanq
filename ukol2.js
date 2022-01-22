5c6226d61a25a469bc3e6fe8838b20309777bd66
zuzanavolfova <zuzka.volfova@gmail.com>
2022-01-22 12:57:04 +0100

    1 file modified

--- ukol2.js
+++ ukol2.js
@@ -1,7 +1,16 @@
 let jmeno = prompt('Jaké je tvé jméno');
 let vek = prompt('Kolik ti je let');
 
-If (vek>18) {
-console.log('Vaše jméno je ' + jmeno + ', je Vám ' + vek + 'let a tak mate pravo ridit auto');
+if (vek > 18) {
+  console.log(
+    'Vaše jméno je ' +
+      jmeno +
+      ', je Vám ' +
+      vek +
+      ' let a tak mate pravo ridit auto'
+  );
+} else {
+  console.log(
+    'Vaše jméno je' + jmeno + ', je Vám ' + vek + ' a tak musíte ještě počkat.'
+  );
 }
-else { console.log( "Vaše jméno je" + jmeno + ", je Vám " + vek + "a tak musíte ještě počkat."  )}
