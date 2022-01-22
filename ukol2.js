b8b4c90087f2d1967f972ebe937e2401bb420027
zuzanavolfova <zuzka.volfova@gmail.com>
2022-01-22 13:16:36 +0100

    1 file modified

--- ukol2.js
+++ ukol2.js
@@ -1,7 +1,8 @@
 let jmeno = prompt('Jaké je tvé jméno');
 let vek = prompt('Kolik ti je let');
+let vypocet = 18 - vek;
 
-if (vek > 18) {
+if (vek >= 18) {
   console.log(
     'Vaše jméno je ' +
       jmeno +
@@ -10,7 +11,20 @@ if (vek > 18) {
       ' let a tak mate pravo ridit auto'
   );
 } else {
+  let vypocet = 18 - vek;
+  if (vypocet===1)
   console.log(
-    'Vaše jméno je' + jmeno + ', je Vám ' + vek + ' a tak musíte ještě počkat.'
+    'Vaše jméno je ' +
+      jmeno +
+      ', je Vám ' +
+      "17 let" +
+      ' a tak musíte ještě rok počkat, než budete moci ridit auto.'
   );
-}
+  else if (vypocet >=2 && vypocet<=4)
+  console.log(
+    'Vaše jméno je ' +
+      jmeno +
+      ', je Vám ' +
+      vek " let" +
+      ' a tak musíte ještě' + vypocet + " roky počkat, než budete moci ridit auto."
+  }
